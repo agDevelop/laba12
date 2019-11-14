6891b3b8faee242e218a91365b7c2ce2a46b25d7
agDevelop <andrewnikolaevich.2014@gmail.com>
2019-11-14 17:04:27 +0300

    New
    New proj

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PersonViewComponent } from './person-view/person-view.component';
+import { PersonAddComponent } from './person-add/person-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonViewComponent,
+    PersonAddComponent
   ],
   imports: [
     BrowserModule
